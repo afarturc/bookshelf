d276210a6eb123d1430fd5a80986f006a8812480
Artur Correia <art.afo@outlook.pt>
2024-02-29 10:32:16 +0000

    now we can display a book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,15 @@
 class BooksController < ApplicationController
+  before_action :set_book, only: %i[show]
+
   def index
     @books = Book.all.order(created_at: :asc)
+  end
+
+  def show; end
+
+  private
+
+  def set_book
+    @book = Book.find(params[:id])
   end
 end
--- spec/requests/books_request_spec.rb
+++ spec/requests/books_request_spec.rb
@@ -12,4 +12,18 @@ RSpec.describe Book, type: :request do
       end
     end
   end
+
+  describe "GET #show" do
+    let(:book) { create(:book) }
+
+    it "returns book details" do
+      get book_path(book)
+
+      aggregate_failures do
+        expect(response).to have_http_status(:ok)
+        expect(response.content_type).to eq("text/html; charset=utf-8")
+        expect(response.body).to include(book.title)
+      end
+    end
+  end
 end
